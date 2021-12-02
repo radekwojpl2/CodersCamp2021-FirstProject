e0e628712a69b2c3de8899c8d210d2a9cb1abbc4
Pawel211 <pawel.chmielek1@gmail.com>
2021-12-02 23:20:30 +0100

    starting building code

--- src/fastFood.js
+++ src/fastFood.js
@@ -30,25 +30,3 @@ const makeDivs = item => {
       newMenuElement.appendChild(newMenuImage);
     }
 }
-
-
-
-
-
-  // const renderMenu = async () => {
-// let menuPieces = await getApi(`https://api.spoonacular.com/food/menuItems/search?query=${query}&number=${number}&apiKey=${apiKey}`)
-// return menuPieces
-// // for (let i=0 ; i<number; i++){
-// //   const newMenuElement = document.createElement('div');
-// //   // newMenuElement.classList.add("menu-element")
-// //   let itemTitle = menuPieces.menuItems[i].title
-// //   newMenuElement.textContent = itemTitle;
-// //   const newMenuImage = document.createElement ('img');
-// //   let imageSrc = menuPieces.menuItems[i].image;
-// //   newMenuImage.src = imageSrc;
-// //   div.appendChild(newMenuElement);
-// //   newMenuElement.appendChild(newMenuImage);
-// // }
-// // console.log(menuPieces)
-// }
-// //  renderMenu()
