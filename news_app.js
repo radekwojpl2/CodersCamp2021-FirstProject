b88f5e61dfc50d5348e5a9a4e707819a0aae4926
Kuba Zakrzewski <j.zakrzewsky@gmail.com>
2021-12-02 21:08:27 +0100

    articles loop

--- news_app.js
+++ news_app.js
@@ -2,7 +2,7 @@
 
 // FETCH VARIABLES
 const topic = 'food';
-const source = `https://newsapi.org/v2/everything?q=${topic}&from=2021-11-02&sortBy=publishedAt&apiKey=b10b22f57fca436ca78791aa2c90a376`;
+const source = `https://newsapi.org/v2/everything?q=${topic}&sortBy=popularity&pageSize=5&apiKey=b10b22f57fca436ca78791aa2c90a376`;
 
 // DOM VARIABLES
 const body = document.body;
@@ -26,41 +26,76 @@ getData()
         console.log(data);
         // TODO: NEEDED DATA OBJECTS: author, content -short, description -long, published -date, source{name}, title, url, urlToImage 
         
-        const {title, author, published, description, source, url, urlToImage} = data.articles[0];
-        
-        // NEW NEWS DIV
-        const newsDiv = document.createElement('div');
-        // ADD IMAGE
-        const newsPic = document.createElement('img');
-        newsPic.src = urlToImage
-        newsDiv.appendChild(newsPic);
-        // ADD TITLE HEADER
-        const newsHead = document.createElement('h3');
-        newsHead.innerText = title;
-        newsDiv.appendChild(newsHead);
-        // ADD AUTHOR
-        const newsAuthor = document.createElement('h4');
-        newsAuthor.innerText = author;
-        newsDiv.appendChild(newsAuthor);
-        // ADD PUBLISHED DATE
-        const newsPublished = document.createElement('h5');
-        newsPublished.innerText = published;
-        newsDiv.appendChild(newsPublished);
-        // ADD NEWS CONTETNT
-        const newsDescription = document.createElement('p');
-        newsDescription.innerText = description;
-        newsDiv.appendChild(newsDescription);
-        // ADD READ MORE LINK
-        const readMore = document.createElement('a');
-        readMore.href = url;
-        readMore.innerText = `Continue reading on ${source.name}`;
-        newsDiv.appendChild(readMore);
+        // ! MULTIPLE ARTICLES LOOP
+        const newsArticles = data.articles;
+
+        newsArticles.forEach(article => {
+            const {title, author, published, description, source, url, urlToImage} = article;
+            // NEW NEWS DIV
+            const newsDiv = document.createElement('div');
+            // ADD IMAGE
+            const newsPic = document.createElement('img');
+            newsPic.src = urlToImage
+            newsDiv.appendChild(newsPic);
+            // ADD TITLE HEADER
+            const newsHead = document.createElement('h3');
+            newsHead.innerText = title;
+            newsDiv.appendChild(newsHead);
+            // ADD AUTHOR
+            const newsAuthor = document.createElement('h4');
+            newsAuthor.innerText = author;
+            newsDiv.appendChild(newsAuthor);
+            // ADD PUBLISHED DATE
+            const newsPublished = document.createElement('h5');
+            newsPublished.innerText = published;
+            newsDiv.appendChild(newsPublished);
+            // ADD NEWS CONTETNT
+            const newsDescription = document.createElement('p');
+            newsDescription.innerText = description;
+            newsDiv.appendChild(newsDescription);
+            // ADD READ MORE LINK
+            const readMore = document.createElement('a');
+            readMore.href = url;
+            readMore.innerText = `Continue reading on ${source.name}`;
+            newsDiv.appendChild(readMore);
+            // APPEND READY DIV
+            news.appendChild(newsDiv);
+        });
         
 
 
-
+        // ! SINGLE ARTICLE STRUCTURE
+        // const {title, author, published, description, source, url, urlToImage} = data.articles[0];
         
-        news.appendChild(newsDiv);
+        // // NEW NEWS DIV
+        // const newsDiv = document.createElement('div');
+        // // ADD IMAGE
+        // const newsPic = document.createElement('img');
+        // newsPic.src = urlToImage
+        // newsDiv.appendChild(newsPic);
+        // // ADD TITLE HEADER
+        // const newsHead = document.createElement('h3');
+        // newsHead.innerText = title;
+        // newsDiv.appendChild(newsHead);
+        // // ADD AUTHOR
+        // const newsAuthor = document.createElement('h4');
+        // newsAuthor.innerText = author;
+        // newsDiv.appendChild(newsAuthor);
+        // // ADD PUBLISHED DATE
+        // const newsPublished = document.createElement('h5');
+        // newsPublished.innerText = published;
+        // newsDiv.appendChild(newsPublished);
+        // // ADD NEWS CONTETNT
+        // const newsDescription = document.createElement('p');
+        // newsDescription.innerText = description;
+        // newsDiv.appendChild(newsDescription);
+        // // ADD READ MORE LINK
+        // const readMore = document.createElement('a');
+        // readMore.href = url;
+        // readMore.innerText = `Continue reading on ${source.name}`;
+        // newsDiv.appendChild(readMore);
+        // // APPEND READY DIV
+        // news.appendChild(newsDiv);
     })
     .catch(err => console.log('runtime error', err.message));
 
