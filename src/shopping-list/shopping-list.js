e8875acd1840a26631418369816e012e0bbd64d3
Agata Rostojek <a.rostojek@gmail.com>
2021-12-07 23:53:04 +0100

    shopping-list: styling updated, remove ingredients button created

--- src/shopping-list/shopping-list.js
+++ src/shopping-list/shopping-list.js
@@ -1,5 +1,7 @@
 const searchInput = document.querySelector('.shoppinglist__search--input--js');
 const displayedRecipiesList = document.querySelector('.shoppinglist__displayed-recipies--js');
+const shoppingListContainer = document.querySelector('.shoppinglist__results--ingredients-container');
+
 
 searchInput.addEventListener('keyup', () => {
     const searchQuery = searchInput.value;
@@ -7,7 +9,7 @@ searchInput.addEventListener('keyup', () => {
     showRecipes(recipes) 
 });
 
-let shoppingList = [];
+const shoppingList = [];
 
 
 const getRecipesFromAPI = (searchQuery) => {
@@ -23,32 +25,68 @@ const showRecipes = (recipes) => {
     displayedRecipiesList.innerHTML = '';
     for(let i = 0; i < recipes.length; i++) {
         const recipeElement = document.createElement('div');
+        recipeElement.className = "shoppinglist__displayed-recipies--element";
         const recipeElementHeader = document.createElement('h5');
-        recipeElementHeader.innerText =  `${recipes[i].id} ${recipes[i].name}`;
+        recipeElementHeader.innerHTML =  `${recipes[i].id} ${recipes[i].name}`;
         const recipeIngredientsContainer = document.createElement('div');
-
         recipeElementHeader.onclick = () => onRecipeClicked(recipeIngredientsContainer, recipes[i].ingredients);
         recipeElement.appendChild(recipeElementHeader);
         recipeElement.appendChild(recipeIngredientsContainer);
         displayedRecipiesList.appendChild(recipeElement);
     }
-}
+};
 
 const onRecipeClicked = (container, ingredients) => {
     if(container.innerHTML === '') {
         for(let i = 0; i < ingredients.length; i++) {
             const ingredientElement = document.createElement('div');
             ingredientElement.innerHTML = `${ingredients[i].id} ${ingredients[i].name}`;
-            const ingredientElementButton = document.createElement('button');
-            ingredientElementButton.innerHTML = `+`;
+            ingredientElement.className = 'shoppinglist__displayed-ingredients'
+            const ingredientAddButton = document.createElement('button');
+            ingredientAddButton.innerHTML = `+`;
+            ingredientAddButton.className = 'shoppinglist__displayed-ingredients--add-button';
 
-            ingredientElementButton.onclick = () =>  console.log('button for ' + ingredients[i].name + ' clicked');
+            ingredientAddButton.onclick = () => addIngredient(ingredients[i]);
             container.appendChild(ingredientElement);
-            ingredientElement.appendChild(ingredientElementButton);
+            ingredientElement.appendChild(ingredientAddButton);
         }
     } else {
         container.innerHTML = '';
     }
+};
+
+
+const addIngredient = (ingredient) => {
+    shoppingList.push(ingredient);
+    updateShoppingList();
+};
+
+updateShoppingList = () => {
+    shoppingListContainer.innerHTML = '';
+    for(let i = 0; i < shoppingList.length; i++) {
+        const shoppingListIngredientElement = document.createElement('div');
+        shoppingListIngredientElement.className = 'shoppinglist__results--ingredient';
+        const shoppingListIngredientHeader = document.createElement('h5');
+        shoppingListIngredientElement.innerText = `${shoppingList[i].name}`;
+
+        const ingredientRemoveButton = document.createElement('button');
+        ingredientRemoveButton.innerHTML = '-';
+        ingredientRemoveButton.className = 'shoppinglist__results--remove-button';
+
+        ingredientRemoveButton.onclick = () => removeIngredient(i);
+        shoppingListContainer.appendChild(shoppingListIngredientElement);
+        shoppingListIngredientElement.appendChild(shoppingListIngredientHeader);
+
+        shoppingListIngredientElement.appendChild(ingredientRemoveButton);
+
+
+
+    } 
 }
 
 
+const removeIngredient = (index) => {
+    console.log(shoppingList)
+    shoppingList.splice(index, 1);
+    updateShoppingList();
+}
