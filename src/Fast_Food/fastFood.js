d4e010045dbe847a6e668ac5897d4cd375ca342a
Pawel211 <pawel.chmielek1@gmail.com>
2021-12-08 19:06:36 +0100

    updated styles

--- src/Fast_Food/fastFood.js
+++ src/Fast_Food/fastFood.js
@@ -1,4 +1,4 @@
-const apiKey = '32a30d5008384c0abc8d37b703d08307';
+const apiKey = 'e0c14164498a428caa1de238781d3420';
 const divTarget = document.getElementById('menus')
 const searchButton = document.getElementById('search')
 
@@ -18,10 +18,10 @@ const getApi = async (url) => {
   }
 }
 
-const renderMenuItems = () => {
+const renderMenu = () => {
   const numberInput = document.getElementById('menu-amount');
   const number = numberInput.value;
-  clearDivs(number);
+  clearDivs();
   const input = document.getElementById('menu-search');
   let query;
   input.value ? query = input.value : query = "menu";
@@ -29,50 +29,52 @@ const renderMenuItems = () => {
   getApi(`https://api.spoonacular.com/food/menuItems/search?query=${query}&number=${number}&apiKey=${apiKey}&addMenuItemInformation=true`)
   .then(data => {
       const arrayMenuItems = data.menuItems;
-
-      arrayMenuItems.forEach((element) => {
-        const menuElement = makeElementWithClass('div', 'menu-element')
-        const titleElement = makeElementWithClass('div', 'menu-element-title');
-        const restaurantName = makeElementWithClass('div', 'menu-element-restaurant')
-        const imgElement = makeElementWithClass('div', 'menu-element-image');
-
-        let itemTitle = element.title;      
-        titleElement.textContent = itemTitle;
-
-        let itemRestaurant = element.restaurantChain;
-        restaurantName.textContent = `Restaurant : ${itemRestaurant}`;
-
-        let imageSrc = element.image;
-        imgElement.style.backgroundImage = `url(${imageSrc})`
-        divTarget.appendChild(menuElement);
-        menuElement.appendChild(titleElement);
-        menuElement.appendChild(restaurantName);
-        menuElement.appendChild(imgElement);
-        
-        renderNutritionInfo(element, menuElement)
-    })
+      arrayMenuItems.forEach((element) => createMenuItems(element))
   });
 }
 
-searchButton.addEventListener('click', renderMenuItems)
+searchButton.addEventListener('click', renderMenu)
 
-const clearDivs = number => {
-  for (let i=0; i< number; i++){
+const clearDivs = () => {
     while(divTarget.firstChild) {
       divTarget.removeChild(divTarget.firstChild);
   }
-  }
+  
+}
+const createMenuItems = element => {
+  const menuElement = makeElementWithClass('div', 'menu-element')
+  const titleElement = makeElementWithClass('div', 'menu-element-title');
+  const restaurantName = makeElementWithClass('div', 'menu-element-restaurant')
+  const imgElement = makeElementWithClass('div', 'menu-element-image');
+
+  let itemTitle = element.title;      
+  titleElement.textContent = itemTitle;
+
+  let itemRestaurant = element.restaurantChain;
+  restaurantName.textContent = `Restaurant : ${itemRestaurant}`;
+
+  let imageSrc = element.image;
+  imgElement.style.backgroundImage = `url(${imageSrc})`
+
+  divTarget.appendChild(menuElement);
+  menuElement.appendChild(imgElement);
+  menuElement.appendChild(titleElement);
+  menuElement.appendChild(restaurantName);
+  renderNutritionInfo(element, menuElement)
 }
 
 const renderNutritionInfo = (element, menuElement) => {
   const arrayNutrition = element.nutrition.nutrients;
   const nutritionInfo = document.createElement('ul');
+  nutritionInfo.innerHTML = "<b>Nutrients:</b> <br><br>"
   arrayNutrition.forEach((skladnik) => {    
     const nutritionName = skladnik.name;
     const nutritionAmount = skladnik.amount;
     const nutritionUnit = skladnik.unit;
+    
     nutritionInfo.innerHTML += `<li>${nutritionName} ${nutritionAmount} ${nutritionUnit}</li>`;       
    })
   menuElement.appendChild(nutritionInfo);
 }
-renderMenuItems();
+
+renderMenu();
