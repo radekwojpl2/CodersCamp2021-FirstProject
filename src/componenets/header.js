d410f9189c2a212d674359a3b2109e0f838b9d38
Jake Zakrzewski <j.zakrzewsky@gmail.com>
2021-12-17 16:02:19 +0100

    haeder component: added list of filess to class

--- src/componenets/header.js
+++ src/componenets/header.js
@@ -1,9 +1,39 @@
-// TEST
-
 class foodAppHeader extends HTMLElement {
   constructor() {
     super();
 
+    this.links = [
+      {
+        name: 'Home',
+        href: 'index.html',
+        active: this.checkPathname('index.html'),
+      },
+      {
+        name: 'Shopping List',
+        href: 'shoppingList.html',
+        active: this.checkPathname('shoppingList.html'),
+      },
+      {
+        name: 'Page',
+        href: 'page.html',
+        active: this.checkPathname('p.html'),
+      },
+      {
+        name: 'Page',
+        href: 'page.html',
+        active: this.checkPathname('p.html'),
+      },
+      {
+        name: 'Page',
+        href: 'page.html',
+        active: this.checkPathname('p.html'),
+      },
+    ];
+  }
+
+  checkPathname(path) {
+    const pathname = window.location.pathname.split('/');
+    return pathname[1] === path;
   }
 
   connectedCallback() {
@@ -11,17 +41,21 @@ class foodAppHeader extends HTMLElement {
     <header class="nav-header">
       <nav class="nav-bar">
         <ul class="nav-list">
-          <li class="nav-item"><a href="#">Home</a></li>
-          <li class="nav-item"><a href="#">Home</a></li>
-          <li class="nav-item"><a href="#">Home</a></li>
-          <li class="nav-item"><a href="#">Home</a></li>
-          <li class="nav-item"><a href="#">Home</a></li>
-          <li class="nav-item"><a href="#">Home</a></li>
+          ${this.links
+            .map(
+              (link) =>
+                `
+            <li class="nav-item ${link.active ? 'nav-item--active' : ''}">
+              <a class="nav-link" href="${link.href}">${link.name}
+            </li>
+            `
+            )
+            .join('')}
         </ul>
       </nav>
     </header>
     `;
   }
-};
+}
 
 customElements.define('component-header', foodAppHeader); // first param is html tag
